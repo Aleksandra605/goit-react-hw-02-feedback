9175cddd49bab58a4f5727a6b685003e1f3abce5
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-08-30 16:40:06 +0200

    added settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import SectionFeedback from './component/SectionFeedback';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SectionFeedback title="Please leave feedback" />;
 }
 
 export default App;
